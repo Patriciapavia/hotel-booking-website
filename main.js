4dabeff25334eaea467aa605ba793cd58498284b
patricia <patriciapavia236@gmail.com>
2020-06-08 12:34:03 +1000

    add media query

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let header = document.querySelector(".header");
 let hamburgerMenu = document.querySelector(".hamburger-menu");
+window.addEventListener("scroll", function () {
+  let windownPosition = window.scrollY > 0;
+  header.classList.toggle("active", windownPosition);
+});
 
 hamburgerMenu.addEventListener("click", function () {
   header.classList.toggle("menu-open");
